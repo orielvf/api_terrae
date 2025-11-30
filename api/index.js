f2a231d40e2a6a29a982a0af78c695a01785a892
Oriel Ferreira <orielvf@gmail.com>
2025-11-30 18:31:58 -0300

    Organização de projeto, deletei os arquivos não utilizados estava usando outro projeto como base

--- api/index.js
+++ api/index.js
@@ -3,30 +3,6 @@ const express = require('express');
 const cors = require('cors');
 
 const loginHospedinRoutes = require('../routes/login_hospedin');
-const enviarCodigoEmailRoutes = require('../routes/enviarCodigoEmail');
-const cadastrarLoginRoutes = require('../routes/cadastrarLogin');
-const loginRoutes = require('../routes/login');
-const cadastrarClienteRoutes = require('../routes/cadastrarCliente');
-const lerClientesRoutes = require('../routes/lerCliente');
-const lerCampanhaRoutes = require('../routes/lerCampanha');
-const editarClienteRoutes = require('../routes/editarCliente');
-const deletarClienteRoutes = require('../routes/deletarCliente');
-const deletarCampanhaRoutes = require('../routes/deletarCampanha');
-const cadastrarCampanhaRoutes = require('../routes/cadastrarCampanha');
-const atualizarCampanhaRoutes = require('../routes/atualizarCampanha');
-const rodarPromotorRoutes = require('../routes/rodarPromotor');
-const rodarNPSRoutes = require('../routes/rodarNPS');
-const atualizarHooksRoutes = require('../routes/atualizarHooks');
-const apoiadorTrueRoutes = require('../routes/apoiadorTrue');
-const promotorTrueRoutes = require('../routes/promotorTrue');
-const retratorTrueRoutes = require('../routes/retratorTrue');
-const lerWebhooksRoutes = require('../routes/lerWebhooks');
-const populaCampanhaNPSRoutes = require('../routes/populaCampanhaNPS');
-const rodarCampanhaNPSRoutes = require('../routes/rodarCampanhaNPS');
-const testeRoutes = require('../routes/teste');
-
-
-
 
 const app = express();
 
@@ -44,29 +20,6 @@ app.use((req, res, next) => {
 
 
 app.use('/login_hospedin', loginHospedinRoutes);
-app.use('/enviarCodigoEmail', enviarCodigoEmailRoutes);
-app.use('/cadastrarLogin', cadastrarLoginRoutes);
-app.use('/login', loginRoutes);
-app.use('/cadastrarCliente', cadastrarClienteRoutes);
-app.use('/lerClientes', lerClientesRoutes);
-app.use('/lerCampanha', lerCampanhaRoutes);
-app.use('/editarCliente', editarClienteRoutes);
-app.use('/deletarCliente', deletarClienteRoutes);
-app.use('/deletarCampanha', deletarCampanhaRoutes);
-app.use('/cadastrarCampanha', cadastrarCampanhaRoutes);
-app.use('/atualizarCampanha', atualizarCampanhaRoutes);
-app.use('/rodarPromotor', rodarPromotorRoutes);
-app.use('/rodarNPS', rodarNPSRoutes);
-app.use('/atualizarHooks', atualizarHooksRoutes);
-app.use('/apoiadorTrue', apoiadorTrueRoutes);
-app.use('/promotorTrue', promotorTrueRoutes);
-app.use('/retratorTrue', retratorTrueRoutes);
-app.use('/lerWebhooks', lerWebhooksRoutes);
-app.use('/populaCampanhaNPS', populaCampanhaNPSRoutes);
-app.use('/rodarCampanhaNPS', rodarCampanhaNPSRoutes);
-app.use('/teste', testeRoutes);
-
-
 
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
